bc11fd0561bd1cf7c4415ec7de09f82eb24a8152
Kirill Kovalev <kirill.kovalev1502@mail.ru>
2021-07-21 14:17:33 +0300

    FirstCommit

---
+++ app/src/main/java/ru/startandroid/develop/imon/MainActivity.kt
@@ -0,0 +1,32 @@
+package ru.startandroid.develop.imon
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.appcompat.widget.Toolbar
+import com.mikepenz.materialdrawer.AccountHeader
+import com.mikepenz.materialdrawer.Drawer
+import ru.startandroid.develop.imon.databinding.ActivityMainBinding
+
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var mBinding: ActivityMainBinding
+    private lateinit var mDrawer: Drawer
+    private lateinit var mHeader: AccountHeader
+    private lateinit var mToolbar: Toolbar
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        mBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(mBinding.root)
+    }
+
+    override fun onStart() {
+        super.onStart()
+        initFields()
+    }
+
+    private fun initFields() {
+        mToolbar = mBinding.mainToolbar
+    }
+}
